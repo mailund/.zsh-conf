ba0067ac031a4b928ab92422d10f5c13ec89bd1f
Thomas Mailund <mailund@birc.au.dk>
2022-11-12 09:15:16 +0100

    Smaller changes to conf.

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+
 ZSH_THEME="minimal"
 
 # Configuring...
