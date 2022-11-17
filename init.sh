4a7dc5577594265bec47697e624855613b177d6f
Thomas Mailund <mailund@birc.au.dk>
2022-11-17 11:03:42 +0100

    mount commands

--- init.sh
+++ init.sh
@@ -23,5 +23,27 @@ plugins=(
 )
 source $ZSH/oh-my-zsh.sh
 
+# Functions
+genome_dk_user=mailund
+genome_dk_mount_dir=~/GenomeDK
+function mount_gdk() {
+    local local_dir=$genome_dk_mount_dir
+    local remote_dir=${genome_dk_user}@login.genome.au.dk:/home/${genome_dk_user}
+
+    if ! ( [[ -d $local_dir ]] || mkdir $local_dir ); then
+        echo "Couldn't create mount dir $local_dir"
+        return 2
+    fi
+
+    sshfs $remote_dir $local_dir                        \
+        -o idmap=none -o uid=$(id -u),gid=$(id -g)      \
+        -o allow_other -o umask=077 -o follow_symlinks
+}
+function unmount_gdk() {
+    umount $genome_dk_mount_dir
+    rmdir $genome_dk_mount_dir
+}
+
+# Aliases
 alias idot="dot -Tpng -Gbgcolor=black -Nfontcolor=white -Nfontsize=26 -Efontcolor=white -Efontsize=26 -Ncolor=white -Ecolor=white | kitty icat --align=left"
 
