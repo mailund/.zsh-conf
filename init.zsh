1e4101adceff6595564f105111367c45c2fb5b14
Thomas Mailund <thomas.mailund@gmail.com>
2025-02-03 07:59:16 +0100

    fixed bat style so it also works on laptop

--- init.zsh
+++ init.zsh
@@ -38,7 +38,7 @@ COMPLETION_WAITING_DOTS="true"
 # Set bat as default pager and set its theme
 export PAGER=bat
 export BAT_THEME=zenburn
-export BAT_STYLE=-numbers,plain
+export BAT_STYLE=plain
 
 # Theme
 ZSH_THEME="af-magic"
