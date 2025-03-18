d011b9d175146925ce78ee3b0e8a410eda01ad09
Thomas Mailund <thomas.mailund@gmail.com>
2025-03-18 10:58:57 +0100

    new gpubox alias

--- alias.zsh
+++ alias.zsh
@@ -6,4 +6,4 @@ alias wdot="dot -Tpng -Gbgcolor=black -Nfontcolor=white -Nfontsize=26 -Efontcolo
 
 # Alias for ssh'ing into the building box on EC2
 alias buildbox="ssh -i ~/.ssh/mailunds_building_box.pem ubuntu@ec2-52-49-70-204.eu-west-1.compute.amazonaws.com"
-
+alias gpubox="ssh -i ~/.ssh/mailunds_building_box.pem ubuntu@ec2-108-128-166-157.eu-west-1.compute.amazonaws.com"
