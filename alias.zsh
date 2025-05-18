763328ce7f5bd18d68ee33bf8ca2fbb52965372a
Thomas Mailund <tm@kvantify.dk>
2025-05-18 12:42:46 +0200

    updated idot to work with new warp

--- alias.zsh
+++ alias.zsh
@@ -1,8 +1,7 @@
 # Aliases #######################################
 
-## Alias for displaying dot files in iTerm
+## Alias for displaying dot files inline
 alias idot="dot -Tpng -Gbgcolor=black -Nfontcolor=white -Nfontsize=26 -Efontcolor=white -Efontsize=26 -Ncolor=white -Ecolor=white | imgcat"
-alias wdot="dot -Tpng -Gbgcolor=black -Nfontcolor=white -Nfontsize=26 -Efontcolor=white -Efontsize=26 -Ncolor=white -Ecolor=white | open -f -a Preview"
 
 # Alias for ssh'ing into the building box on EC2
 alias buildbox="ssh -i ~/.ssh/mailunds_building_box.pem ubuntu@ec2-52-49-70-204.eu-west-1.compute.amazonaws.com"
